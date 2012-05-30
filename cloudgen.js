2ad660ee99bec7763d52832e239e5f72bba04b95
Ben Pryke <ninjakannon@hotmail.com>
2012-05-30 05:38:29 +0100

    Added comments, made drawCloudGroup compatible with missing arguments

--- cloudgen.js
+++ cloudgen.js
@@ -1,6 +1,6 @@
 // TODO: Add license.
 
-// Hide our creation of cloudgen from the global scope.
+// Hide the cloudgen scope from the global scope in an anonymous function.
 (function() {
     // Constants =============================================================
     // We require this later, so precompute it here.
@@ -10,43 +10,48 @@
     // the clouds are thus this times the cloud radius.
     var CIRCLE_RADIUS_RATIO = 0.6;
     
-    // The spacing between 
+    // The spacing as a percentage of cloud radius between clouds in the grid
+    // of "drawCloudGroup".
     var CLOUD_GROUP_SPACING = 0.75;
     
     
+    
+    // Setup cloudgen ========================================================
     // Create our cloudgen object.
     var cloudgen = function() {
     };
     
     
+    
+    // Public cloudgen Methods ===============================================
     /*  Draws a single cloud to the given canvas context.
         
-        context: (Required)
+        context (required)
             The 2D "CanvasRenderingContext2D" instance to draw to.
             
-        centreX: (Optional)
-            The approximate centre of the cloud in the x-dimension. Default 0.
+        centreX (optional) Default 0.
+            The approximate centre of the cloud in the x-dimension.
             
-        centreY: (Optional)
-            The approximate centre of the cloud in the y-dimension. Default 0.
+        centreY (optional) Default 0.
+            The approximate centre of the cloud in the y-dimension.
             
-        radius: (Optional)
+        radius (optional) Default 100.
             The radius of the circular area inside which the cloud will be
-            generated. Default 100.
+            generated.
             
-        colour: (Optional)
+        colour (optional) Default {r:255, g:255, b:255}.
             An object of the form {r:0, g:0, b:0} representing the RGB values
-            of the cloud colour. Default {r:255, g:255, b:255}.
+            of the cloud colour.
             
-        alpha: (Optional)
-            The transparency of the cloud. Range 0.0 to 1.0. Default 0.3.
+        alpha (optional) Default 0.3.
+            The transparency of the cloud. Range 0.0 to 1.0.
             
-        circles: (Optional)
+        circles (optional) Default 30.
             Clouds are created by drawing numerous gradient-filled circles;
             the more there are, the thicker the cloud. This is the number to
-            draw. Default 30. */
+            draw. */
     cloudgen.drawCloud = function(context, centreX, centreY, radius, colour, alpha, circles) {
-        // Set default parameters.
+        // Set default arguments.
         switch (arguments.length) {
             case 0: return;
             case 1: centreX = 0;
@@ -93,24 +98,65 @@
     };
     
     
-    /*
-    topLeftX: centre of top-left cloud.
-    */
-    cloudgen.drawCloudGroup = function(context, topLeftX, topLeftY, grid, radius, colour, alpha, circles) {
-        // Loop through the cloud positions array.
+    /*  Draws a grid of clouds to the given canvas context.
+        
+        context (required)
+            The 2D "CanvasRenderingContext2D" instance to draw to.
+        
+        grid (required)
+            A two-dimensional binary array specifying the location of clouds
+            with ones. Note that rows can be different lengths. For example, a
+            grid specifying a single diagonal cloud is below.
+                [[1],
+                 [0, 1],
+                 [0, 0, 1]]
+        
+        topLeftX (optional) Default 0.
+            The approximate centre of the top-left cloud centre in the grid in
+            the x-dimension.
+        
+        topLeftY (optional) Default 0.
+            The approximate centre of the top-left cloud centre in the grid in
+            the y-dimension.
+        
+        radius: see "drawCloud".
+        colour: see "drawCloud".
+        alpha: see "drawCloud".
+        circles: see "drawCloud". */
+    cloudgen.drawCloudGroup = function(context, grid, topLeftX, topLeftY, radius, colour, alpha, circles) {
+        // Set default arguments.
+        switch (arguments.length) {
+            case 0: return;
+            case 1: return;
+            case 2: topLeftX = 0;
+            case 3: topLeftY = 0;
+            case 4: radius = 100;
+        }
+        
+        // Get the parameters that we need to sent to "drawCloud".
+        var parameters = Array.prototype.splice.call(arguments, 4);
+        
+        // Loop through the cloud grid, row by row.
         for (var i = 0; i < grid.length; i++) {
             for (var j = 0; j < grid[i].length; j++) {
+                // If this cell is a 1, we draw a cloud.
                 if (grid[i][j] === 1) {
+                    // Calculate the cloud centre position.
                     var centreX = topLeftX + j * radius * CLOUD_GROUP_SPACING;
                     var centreY = topLeftY + i * radius * CLOUD_GROUP_SPACING;
                     
-                    this.drawCloud(context, centreX, centreY, radius, colour, alpha, circles);
+                    // Call "drawCloud" with the correct parameters as passed
+                    // to this method. Parameters that were not passed will
+                    // not be sent.
+                    this.drawCloud.apply(this, [context, centreX, centreY].concat(parameters));
                 }
             }
         }
     };
     
     
+    
+    // Finalisation ==========================================================
     // Expose cloudgen to the global scope.
     window.cloudgen = cloudgen;
 })();
